c1af3045aff39855c95c427938141d13e6aaac3e
saruhasansankar <165461195+saruhasansankar@users.noreply.github.com>
2024-05-29 11:21:06 +0530

    code formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ImageViewer from './component/360ImageViewer';
+import React from "react";
+import ImageViewer from "./component/360ImageViewer";
 
 function App() {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-
+    <div style={{ width: "100vw", height: "100vh" }}>
       <ImageViewer />
     </div>
   );
